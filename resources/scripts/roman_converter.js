aaf7cce109bee07333491126bba862b49589b1b9
Lukas Marquardt <lukas@pantaflix.com>
2019-02-18 13:17:45 +0100

    added arrays and toRoman function

--- resources/scripts/roman_converter.js
+++ resources/scripts/roman_converter.js
@@ -1,28 +1,38 @@
 //roman/decimal converter by eljotem 2019 => https://github.com/ElJotEm/Roman-Decimal_Converter
 
-// TODO: mapping arrays
-const roman = new Array;
-const decimal = new Array;
+const roman = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"]
+const decimal = [1000,900,500,400,100,90,50,40,10,9,5,4,1]
 
 // TODO: converter functions
-function toArabic(inputStr) {};
-function toRoman(inputNum) {};
+function toArabic(inputStr) {}
+
+function toRoman(inputNum) {
+    var converted = '';
+    for (var i = 0; i <= decimal.length; i++) {
+        while (inputNum%decimal[i] < inputNum) {
+            converted += roman[i];
+            inputNum -= decimal[i];
+        }
+    }
+    return converted;
+}
 
 function convert() {
     var input = document.getElementById('converterValue'),
         inputValue = input.value.replace(/[,]|[.]/g, '').trim(); //clean up input
 
-    if(!paseInt(inputValue,10)) {
+    if(!parseInt(inputValue,10)) { //check if input is a number or string
         for(var i = 0; i < inputValue.length; i++) {
             var char = inputValue.charAt(i).toUpperCase();
-            if(!roman.includes(char)) {
+            if(!roman.includes(char)) { //check if input is a valid roman numeral
                 alert('Roman numerals can only be I, V, X, L, C, D or M');
                 return false;
             }
         }
-
         input.value = toArabic(inputValue);
-    } else {
+    } else if(inputValue < 4000) {
         input.value = toRoman(inputValue);
+    } else {
+        alter('Values higher than 3.999 are not supported for the common roman numerals.')
     }
 }
